566fd6d9989a5bc2c1fe7ed1a40eb4ed3783c630
ltf <18235121656@163.com>
2021-01-14 17:09:16 +0800

    优化

--- flask_restful/api.py
+++ flask_restful/api.py
@@ -1,40 +1,59 @@
-from flask import Flask
-from flask_restful import Api, Resource
-import request
+from flask import Flask,request
+from flask_restful import Api, Resource,reqparse
 
 app = Flask(__name__)
 api = Api(app)
+parse = reqparse.RequestParser()
 
-users={
-    "1":{
-        "username":"bob",
-        "age":23,
-        "sex":"female",
-        "phone":13548526584
+users=[
+    {
+        'userid':1,
+        'username':'bob',
+        'age':18,
+        'sex':'female',
     },
-    "2":{
-        "username":"bob",
-        "age":18,
-        "sex":"male",
-        "phone":13548526584
+    {
+        'userid':2,
+        'username':'rose',
+        'age':19,
+        'sex':'male',
     }
-}
+]
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 class USERS(Resource):
-    def get(self,user_id):
-        one_user=users.get(user_id)
-        return one_user
+    def get(self,userid):
+        res=None
+        for i in users:
+            if str(i.get('userid'))==str(userid):
+                res=i
+        return res
 
-    def post(self,user_id):
-        print(request.form['data'])
-        return ok
+    def post(self,userid):
+        userid=request.form['userid']
+        username=request.form['username']
+        age=request.form['age']
+        sex=request.form['sex']
+        new_user={'userid':userid,'username':username,'age':age,'sex':sex}
+        users.append(new_user)
+        return users
+
+    def put(self,userid):
+        userid=request.form['userid']
+        age=request.form['age']
+        for i in users:
+            if str(i.get('userid'))==str(userid):
+                i['age']=age
+        return users
+
+    def delete(self,userid):
+        pass
 
 api.add_resource(HelloWorld, '/')
-api.add_resource(USERS, '/users/<user_id>')
+api.add_resource(USERS, '/users/<userid>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3030, debug=True)
--- flask_restful/test_api.py
+++ flask_restful/test_api.py
@@ -1,28 +1,56 @@
 import unittest
-
 from json import dumps
-from requests import get, post
 
-host='http://localhost:3030'
+from requests import get, post,put,delete
+
+host = 'http://localhost:3030'
+
 
 class TestApi(unittest.TestCase):
     def hello_world_get(self):
-        url= f'{host}'
-        res=get(url).json()
+        url = f'{host}'
+        res = get(url).json()
         return res
 
     def user_get(self):
-        url= f'{host}/users/1'
-        res=get(url).json()
+        url = f'{host}/users/1'
+        res = get(url).json()
+        return res
+
+    def user_put(self):
+        url=f'{host}/users/2'
+        data = {
+                "userid":2,
+                "age": 18,
+            }
+        res=put(url,data=data).json()
         return res
 
     def user_post(self):
-        data={}
-        pass
+        url=f'{host}/users/3'
+        data = {
+                "userid":3,
+                "username": "jack",
+                "age": 17,
+                "sex": "male",
+            }
+        res=post(url,data=data).json()
+        return res
+
+    def user_delete(self):
+        url=f'{host}/users/2'
+        data={
+            "userid":2
+        }
+        res=delete(url,data=data).json()
+        return res
 
     def test_all(self):
-        print(self.hello_world_get())
-        print(self.user_get())
+        # print(self.hello_world_get())
+        # print(self.user_get())
+        print(self.user_put())
+        # print(self.user_post())
+
 
 if __name__ == "__main__":
     unittest.main()
